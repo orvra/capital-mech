8deeefae2e96268106be555f30b0a904e7eb2963
orvra <135913026+orvra@users.noreply.github.com>
2023-11-26 14:54:06 -0500

    beta
    browserRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/capital-underground">
+    <BrowserRouter basename="/capital-mech">
       <App />
     </BrowserRouter>
   </React.StrictMode>
